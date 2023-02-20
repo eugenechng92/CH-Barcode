432d6cdbc3bb9539ebd3d14a42ca806c314bed29
Eugene <eugene-chng@hotmail.com>
2023-02-20 14:16:08 +0800

    Create repo

---
+++ BarcodeScan/BarcodeScan/Controller.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+
+namespace BarcodeScan
+{
+    class Controller
+    {
+
+
+        public string partnum;
+        public DateTime time;
+        public Controller(string partnum, DateTime time)
+        {
+            this.partnum = partnum;
+            this.time = time;
+        }
+
+
+        public void savecsv(string partnum, DateTime time)
+        {
+            StringBuilder output = new StringBuilder();
+            string seperator = ",";
+            var timen = time.ToString();
+            string path = @"C:\Users\Eugene\Desktop\Business\BarcodeScan\DATA\data.csv";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter file = new StreamWriter(path))
+                {
+                    String[] headings = { "PartNumber", "System Time" };
+                    output.AppendLine(string.Join(seperator, headings));
+                    var t = output.ToString();
+                    file.WriteLine(output.ToString(), ""); // "sb" is the StringBuilder
+
+                }
+            }
+            using (StreamWriter file = File.AppendText(path))
+            {
+                string[] data = { partnum, timen };
+                output.AppendLine(string.Join(seperator, data));
+                file.WriteLine(output.ToString());
+            }
+
+
+
+        }
+    }
+
+}
---
+++ BarcodeScan/BarcodeScan/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace BarcodeScan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Controller cs;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left) 
+            {
+                this.DragMove();
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+        private void OnKeyDownHandler(object sender, KeyEventArgs e) 
+        {
+            if (e.Key == Key.Return)
+            {
+                DateTime time = DateTime.Now;
+                BarcodeScan.Text =  InputTextbox.Text;
+                LastScanned1.Text = BarcodeScan.Text+" "+time;
+                cs = new Controller(BarcodeScan.Text, time);
+                cs.savecsv(BarcodeScan.Text,time);
+            }
+        }
+
+        
+
+    }
+}
